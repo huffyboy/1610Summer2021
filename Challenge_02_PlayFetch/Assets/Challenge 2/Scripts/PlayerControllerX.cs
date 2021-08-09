c2b4d49a4b42f57bde10bf79f8cb4974f2cbcbba
Michael Huff <huff600@hotmail.com>
2021-08-09 15:07:01 -0600

    Challenge 2 Fetch
    Fixed a game of sending dogs to catch balls

---
+++ Challenge_02_PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private const float spawnDelay = 1.5f;
+    private float spawnTimer = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && spawnTimer > spawnDelay)
+        {
+            spawnTimer = 0.0f;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
